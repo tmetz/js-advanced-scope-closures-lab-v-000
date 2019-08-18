67e1075c3a4cda2f66d9aa30d48d0450235b6de4
Tammy Metz <tammy@tammymetz.com>
2019-08-18 03:03:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function produceDrivingRange(range) {
   return function(beginSt, endSt) {
-    let driveLength = parseInt(beginSt.slice(-2), 10) - parseInt(endSt.slice(-2), 10)
+    let driveLength = parseInt(beginSt.slice(-2), 10) - parseInt(endSt.slice(-2), 10);
     if (range - driveLength >= 0) {
       return "within range by " + range - driveLength;
     }
